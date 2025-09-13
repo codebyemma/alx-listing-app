62e828ca9b69f3a2d057786bcbbb002acf48a9c6
codebyemma <mysubbacc@gmail.com>
2025-09-13 12:04:02 +0100

    done

--- pages/components/common/Card.tsx
+++ pages/components/common/Card.tsx
@@ -3,10 +3,12 @@ import { CardProps } from "../../../interfaces";
 
 const Card : React.FC<CardProps> = ({ title, description, image}) => {
     return (
-        <div>
-            <img src={image} alt={title} />
-            <h2>{title}</h2>
-            <p>{description}</p>
+        <div className="max-w-sm rounded-lg overflow-hidden bg-white shadow-md dark:bg-slate-800">
+            <img className="w-full h-48 object-cover" src={image} alt={title} />
+            <div className="p-4">
+                <h2 className="text-lg font-semibold text-gray-900 dark:text-gray-100">{title}</h2>
+                <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">{description}</p>
+            </div>
         </div>
     );
 }
